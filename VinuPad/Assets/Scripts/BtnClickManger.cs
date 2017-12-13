069014906b6deab657ca14d702f24635c45e304a
ISHNN <cereal032@gmail.com>
2017-12-13 22:38:13 +0900

    Re:Create Project

---
+++ VinuPad/Assets/Scripts/BtnClickManger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnClickManger : MonoBehaviour {
+    public Button[] buttons;
+
+    public AudioClip[] audioClips;
+    //public AudioSource audioSourse;
+    public AudioSource[] audioSources;
+
+    private void Awake()
+    {
+        buttons = GetComponentsInChildren<Button>();
+        audioClips = Resources.LoadAll<AudioClip>("Audio/");
+
+        //audioSourse = GetComponent<AudioSource>();
+
+        audioSources = new AudioSource[buttons.Length];
+        for (int i=0; i<buttons.Length; i++)
+        {
+            audioSources[i] = buttons[i].GetComponent<AudioSource>();
+            audioSources[i].clip = ((i < audioClips.Length) ? audioClips[i] : null);
+
+            SetButtonLisitener(i);
+        }
+        
+        //audioSources = Resources.LoadAll<AudioSource>("AudioSource/");
+    }
+
+    public void SetButtonLisitener(int num)
+    {
+        buttons[num].onClick.AddListener(() => BottonClieked(num));
+    }
+
+    public void BottonClieked(int num)
+    {
+        if (audioSources[num].isPlaying)
+        {
+            audioSources[num].Stop();
+        }
+        else
+        {
+            audioSources[num].Play();
+        }
+    }
+}
---
+++ VinuPad/Assets/Scripts/Metro.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Metro : MonoBehaviour {
+
+    public Image metro;
+
+    private void Awake()
+    {
+        metro = GetComponentInChildren<Image>();
+    }
+    
+    void Start () {
+        StartCoroutine("BPMTimer", 246);
+	}
+
+    IEnumerator BPMTimer (float bpm)
+    {
+        WaitForSeconds sec = new WaitForSeconds(60/bpm);
+        bool light = false;
+
+        while(true){
+            if (light) light = false;
+            else light = true;
+
+            metro.gameObject.SetActive(light);
+
+            yield return sec;
+        }
+    }
+}
