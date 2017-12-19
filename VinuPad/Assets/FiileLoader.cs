6ab386f9e4bb8a8aa0c72c27e00d9d040675446b
ISHNN <cereal032@gmail.com>
2017-12-19 12:29:18 +0900

    Optimized Project(a little)

---
+++ VinuPad/Assets/FiileLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public class FiileLoader : MonoBehaviour {
+    AudioClip[] audioClips;
+
+    BinaryFormatter bf;
+    FileStream file;
+
+    // Use this for initialization
+    void Start () {
+        audioClips = Resources.LoadAll<AudioClip>("Audio/");
+
+        bf = new BinaryFormatter();
+
+        for (int i=0; i < audioClips.Length; i++)
+        {
+
+        }
+    }
+}
